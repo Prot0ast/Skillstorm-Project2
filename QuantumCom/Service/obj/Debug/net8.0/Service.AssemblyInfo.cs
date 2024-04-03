0057ca4df0db3e173f112fc0adac3cc8299176c0
Prot0ast <a_barragan1@u.pacific.edu>
2024-04-03 14:52:22 -0500

    Added all missing project references to continue with DTO development

--- QuantumCom/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ QuantumCom/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f13846b5134330923e65d9e9d01126571960a75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da64de861185d7eec7d6912c7eb5fb7dd6d801d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
