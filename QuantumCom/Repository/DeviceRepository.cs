0f8e4c4719cb9ab0abe5c33b86640ea873f703ff
Prot0ast <a_barragan1@u.pacific.edu>
2024-04-04 15:01:26 -0500

    Implemented device repo

--- QuantumCom/Repository/DeviceRepository.cs
+++ QuantumCom/Repository/DeviceRepository.cs
@@ -5,11 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using Entities;
 using Contracts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository
 {
     public class DeviceRepository : RepositoryBase<Device>, IDeviceRepository
     {
         public DeviceRepository(RepositoryContext repositoryContext) : base(repositoryContext) { }
+
+        public void CreateDevice(Device device) => Create(device);
+
+        public void DeleteDevice(Device device) => Delete(device);
+
+        public async Task<IEnumerable<Device>> GetAllDevices(bool trackChanges) => await FindAll(trackChanges).OrderBy(d => d.Id).ToListAsync();
+
+        public async Task<Device> GetDeviceById(int id, bool trackChanges) => await FindByCondition(d => d.Id.Equals(id), trackChanges).SingleOrDefaultAsync();
     }
 }
