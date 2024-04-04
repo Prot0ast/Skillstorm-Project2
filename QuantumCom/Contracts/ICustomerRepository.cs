9b8c52f67e2cec01f3beafaffce7d43dcc6941b2
Prot0ast <a_barragan1@u.pacific.edu>
2024-04-04 15:17:31 -0500

    Implemented customer repo

--- QuantumCom/Contracts/ICustomerRepository.cs
+++ QuantumCom/Contracts/ICustomerRepository.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities;
 
 namespace Contracts
 {
     public interface ICustomerRepository
     {
+        Task<IEnumerable<Customer>> GetAllCustomers(bool trackChanges);
+        Task<Customer> GetCustomerById(int id, bool trackChanges);
+        void CreateCustomer(Customer customer);
+        void DeleteCustomer(Customer customer);
     }
 }
