6760970e3fb34cb51405e7997f028e78e382030d
Prot0ast <a_barragan1@u.pacific.edu>
2024-04-04 14:47:54 -0500

    Implemented plan repo

--- QuantumCom/Contracts/IPlanRepository.cs
+++ QuantumCom/Contracts/IPlanRepository.cs
@@ -8,7 +8,13 @@ using Entities;
 namespace Contracts
 {
     public interface IPlanRepository
-    { }
-
+    { 
+        Task<IEnumerable<Plan>> GetAllPlans(bool trackChanges);
+        Task<Plan> GetPlanByPrice(decimal price, bool trackChanges);
+        Task<Plan> GetPlanByDeviceLimit(int deviceLimit, bool trackChanges);
+        Task<Plan> GetPlanByName(string name, bool trackChanges);
+        Task<Plan> GetPlanById(int id, bool trackChanges);
+        void CreatePlan(Plan plan);
+        void DeletePlan(Plan plan);
     }
 }
--- QuantumCom/Repository/PlanRepository.cs
+++ QuantumCom/Repository/PlanRepository.cs
@@ -5,11 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using Entities;
 using Contracts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository
 {
     public class PlanRepository : RepositoryBase<Plan>, IPlanRepository
     {
         public PlanRepository(RepositoryContext repositoryContext): base(repositoryContext) {}
+
+        public void CreatePlan(Plan plan) => Create(plan);
+        public void DeletePlan(Plan plan) => Delete(plan);
+        public async Task<IEnumerable<Plan>> GetAllPlans(bool trackChanges) => await FindAll(trackChanges).OrderBy(p => p.Id).ToListAsync();
+
+        public async Task<Plan> GetPlanByDeviceLimit(int deviceLimit, bool trackChanges) => await FindByCondition(p => p.DeviceLimit.Equals(deviceLimit), trackChanges).SingleOrDefaultAsync();
+
+        public async Task<Plan> GetPlanById(int id, bool trackChanges) => await FindByCondition(p => p.Id.Equals(id), trackChanges).SingleOrDefaultAsync();
+
+
+        public async Task<Plan> GetPlanByName(string name, bool trackChanges) => await FindByCondition(p => p.Name.Equals(name), trackChanges).SingleOrDefaultAsync();
+
+        public async Task<Plan> GetPlanByPrice(decimal price, bool trackChanges) => await FindByCondition(p => p.Price.Equals(price), trackChanges).SingleOrDefaultAsync();
+
     }
 }
