079a4c9c90b8e79276a1d174a047cacc7bceb76d
Prot0ast <a_barragan1@u.pacific.edu>
2024-04-05 14:32:21 -0500

    Implemented the device controller (commented out, waiting for service layer)

--- QuantumCom/QuantumCom.Presentation/Controllers/PlanController.cs
+++ QuantumCom/QuantumCom.Presentation/Controllers/PlanController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace QuantumCom.Presentation.Controllers
 {
