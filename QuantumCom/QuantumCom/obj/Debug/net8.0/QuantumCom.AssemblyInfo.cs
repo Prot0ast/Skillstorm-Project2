ae10f2aacd01a44ea92a874aba4397a8f576fdc0
KriswithaK1 <kristopherrobinson2001@gmail.com>
2024-04-12 17:43:54 -0400

    Working

--- QuantumCom/QuantumCom/obj/Debug/net8.0/QuantumCom.AssemblyInfo.cs
+++ QuantumCom/QuantumCom/obj/Debug/net8.0/QuantumCom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantumCom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9539d4668d3789199d32035caa4eab432ddf3d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ea79647b06c271e15c98c38d74cff330344156")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantumCom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantumCom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
