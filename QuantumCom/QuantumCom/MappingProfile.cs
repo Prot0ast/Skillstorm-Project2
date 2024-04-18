17f00fa2fbf105c0dee63a1dd6cd786afb77e096
KriswithaK1 <kristopherrobinson2001@gmail.com>
2024-04-18 16:35:32 -0400

    Mappings config

--- QuantumCom/QuantumCom/MappingProfile.cs
+++ QuantumCom/QuantumCom/MappingProfile.cs
@@ -16,13 +16,24 @@ namespace QuantumCom
             CreateMap<Customer, CustomerDto>()
                 .ForMember(c => c.FullName, 
                 opt => opt.MapFrom(src=> $"{src.FirstName} {src.LastName}"));
-            CreateMap<Billing, BillingDto>();
-            CreateMap<UserForRegistrationDto, User>();
-            CreateMap<CustomerForCreationDto, Customer>();
+            CreateMap<Billing, BillingDto>().ReverseMap();
+            CreateMap<BillingForCreationDto, Billing>().ReverseMap();
+   
+            CreateMap<UserForRegistrationDto, User>().ReverseMap();
+            CreateMap<CustomerForCreationDto, Customer>().ReverseMap();
             CreateMap<CustomerForUpdateDto, Customer>().ReverseMap();
-            CreateMap<CustomerPlan, CustomerPlanDto>();
-            CreateMap<Plan, PlanDto>();
-            CreateMap<Device, DeviceDto>();
+
+            CreateMap<CustomerPlan, CustomerPlanDto>().ReverseMap();
+            CreateMap<CustomerPlanDto, CustomerPlanDto>().ReverseMap();
+            CreateMap<CustomerPlanForCreationDto, CustomerPlanDto>().ReverseMap();
+
+            CreateMap<Plan, PlanDto>().ReverseMap();
+            CreateMap<PlanForCreationDto, Plan>().ReverseMap();
+
+
+            CreateMap<DeviceForCreationDto, Device>().ReverseMap();
+            CreateMap<DeviceForUpdateDto, Device>().ReverseMap();
+            CreateMap<Device, DeviceDto>().ReverseMap();
         }
 
     }
